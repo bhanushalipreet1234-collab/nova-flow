31a7d166cb6744ffb484c5a68566e47b8bc7b191
bhanushalipreet1234-collab <bhanushalipreet1234@gmail.com>
2025-08-09 01:47:49 +0000

    commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Sidebar from "./Sidebar";
 import Editor from "./Editor";
+import { ReactFlowProvider } from "reactflow";
+import "reactflow/dist/style.css";
 
 export default function App() {
   return (
-    <div style={{ display: "flex" }}>
-      <Sidebar />
-      <Editor />
-    </div>
+    <ReactFlowProvider>
+      <div style={{ display: "flex" }}>
+        <Sidebar />
+        <Editor />
+      </div>
+    </ReactFlowProvider>
   );
 }
